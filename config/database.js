af76f5573831328e24b921f86884b2ab689ce04f
Rikcho Hermawan <rikohrmawan258@gmail.com>
2026-07-21 02:57:56 +0700

    Update Connection

--- config/database.js
+++ config/database.js
@@ -1,12 +1,16 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'delima_psb',
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'rikcho',
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? { connectionString: process.env.DATABASE_URL }
+    : {
+        host: process.env.PGHOST || 'localhost',
+        port: process.env.PGPORT || 5432,
+        database: process.env.PGDATABASE || 'delima_psb',
+        user: process.env.PGUSER || 'postgres',
+        password: process.env.PGPASSWORD || 'rikcho',
+      }
+);
 
 module.exports = pool;
